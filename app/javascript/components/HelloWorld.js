6aad0e056aed711987f4307cab7b177e9253f321
erikolsen <erik.s.olsen@gmail.com>
2019-01-05 16:47:07 -0600

    Adds users and tailwind

--- app/javascript/components/HelloWorld.js
+++ app/javascript/components/HelloWorld.js
@@ -3,9 +3,9 @@ import PropTypes from "prop-types"
 class HelloWorld extends React.Component {
   render () {
     return (
-      <React.Fragment>
+      <h1>
         Greeting: {this.props.greeting}
-      </React.Fragment>
+      </h1>
     );
   }
 }
